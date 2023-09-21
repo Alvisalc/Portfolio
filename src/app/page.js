047a638c763b0c707266e84106c361f8119080d7
Alvisalc <alvisccy1107@gmail.com>
2023-09-20 22:55:07 -0400

    about

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import About from "./components/About";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
     <Navbar />
     <div class="container mt-24 mx-auto px-12 py-4">
     <HeroSection />
+    <About />
     </div>
     </main>
   );
