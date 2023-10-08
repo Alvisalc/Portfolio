97697dae8bee08a15c1e69c1175c27fe3e5612d5
Alvisalc <alvisccy1107@gmail.com>
2023-10-08 13:40:32 -0400

    fix bugs

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 
 const About = () => {
   return (
     <section className="" id="about">
         <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-            <Image src="/images/about-image.png" width={500} height={500} alt='hero-image' />
+            <Image src="/images/about-image.png" alt="hero-image" width={500} height={500} />
             <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
                 <h2 className="text-4xl font-bold mb-4">About Me</h2>
                     <p className="text-base lg:text-lg">
