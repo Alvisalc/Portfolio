09ece4d249e5e4a0b5d27b0ce2028d660ef2127c
Alvisalc <alvisccy1107@gmail.com>
2023-10-08 13:37:00 -0400

    fix bugs

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <section className="" id="about">
         <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-            <Image src="/images/about-image.png" width={500} height={500} />
+            <Image src="/images/about-image.png" width={500} height={500} alt='hero-image' />
             <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
                 <h2 className="text-4xl font-bold mb-4">About Me</h2>
                     <p className="text-base lg:text-lg">
@@ -14,7 +14,7 @@ const About = () => {
                     including HTML, CSS, JavaScript, React, Node.js, Express, and Git.
                     I&apos;m also a creative thinker and a team player. 
                     I relish the opportunity to collaborate with others, leveraging our collective skills to bring 
-                    amazing applications to life. Let's work together and create something extraordinary! 🚀
+                    amazing applications to life. Let&apos;s work together and create something extraordinary! 🚀
                     </p>
             </div>
         </div>
